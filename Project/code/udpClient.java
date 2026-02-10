7ba2072f6edc222e2ec462610d23fa872ab0176e
JoeP05-05 <jtperaza05@gmail.com>
2026-02-10 14:24:24 -0600

    Update and rename udp.java to udpClient.java

---
+++ Project/code/udpClient.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
+
+public class udpClient()
+{
+    public static void main(String args[])
+    {
+
+
+
+  
+    }
+}
