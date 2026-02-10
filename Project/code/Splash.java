7b28342cf82e834363b8a1fa64f4b0ad20857a54
JoeP05-05 <jtperaza05@gmail.com>
2026-02-09 22:05:14 -0600

    Added basic methods and import statements

--- Project/code/Splash.java
+++ Project/code/Splash.java
@@ -1,1 +1,17 @@
+import java.io.IOException;
+import javax.swing.JFrame;
+import java.awt.Toolkit;
+
+
+
+public class Splash extends JFrame
+{
+
+}
+
+
+public static void main(String[] args)
+{
+    
+}
 
