f65a8cb722a2c79e3f87b16d7332c3765b309064
JoeP05-05 <jtperaza05@gmail.com>
2026-02-10 14:29:18 -0600

    Create udpServer.java and added basic code

---
+++ Project/code/udpServer.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+
+public class udpServer()
+{
+
+    public static void main(String[] args)
+    {
+          //Make it listen to port 7501
+          DatagramSocket ds = new DatagramSocket(7501)
+
+
+    }
+
+
+  
+
+}
+
+
+
+
+
+
+
+
