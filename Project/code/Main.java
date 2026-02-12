13e64334d9e7e0629b6f6fbc2f8f2b274f900b44
Taija797 <techwithtai@gmail.com>
2026-02-12 14:20:37 -0600

    Add Main class with splash and player entry screens

---
+++ Project/code/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Show splash screen
+        Splash splashScreen = new Splash();
+
+        // After splash screen closes, show player entry screen
+        PlayerEntry playerEntry = new PlayerEntry();
+    }
+}
