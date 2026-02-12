d5a6f88c47f267412f9883ee4d378301c31cc091
JoeP05-05 <jtperaza05@gmail.com>
2026-02-12 14:45:54 -0600

    added server information

--- Project/code/udpServer.java
+++ Project/code/udpServer.java
@@ -12,7 +12,47 @@ public class udpServer()
     {
           //Make it listen to port 7501
           DatagramSocket ds = new DatagramSocket(7501);
-          
+
+          byte[] receive = new byte[65535];
+
+          DatagramPacket serverReceive = null;
+
+          while (true)
+          {
+            // Step 2 : create a DatgramPacket to receive the data.
+			DpReceive = new DatagramPacket(receive, receive.length);
+
+			// Step 3 : revieve the data in byte buffer.
+			ds.receive(DpReceive);
+
+			System.out.println("Client:-" + data(receive));
+
+			// Exit the server if the client sends "bye"
+			if (data(receive).toString().equals("bye"))
+			{
+				System.out.println("Client sent bye.....EXITING");
+				break;
+			}
+
+			// Clear the buffer after every message.
+			receive = new byte[65535];
+		}
+	}
+
+	// A utility method to convert the byte array
+	// data into a string representation.
+	public static StringBuilder data(byte[] a)
+	{
+		if (a == null)
+			return null;
+		StringBuilder ret = new StringBuilder();
+		int i = 0;
+		while (a[i] != 0)
+		{
+			ret.append((char) a[i]);
+			i++;
+		}
+		return ret;
 
         
 
