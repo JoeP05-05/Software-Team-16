1e2dfbfc2905c0df838862fc48837832398719da
JossJongewaard <jossj@uark.edu>
2026-03-05 13:20:52 -0600

    Update install.sh

--- Project/code/install.sh
+++ Project/code/install.sh
@@ -2,15 +2,23 @@
 
 echo "Updating package lists..."
 sudo apt update
+echo
+echo
 
 echo "Installing Java JDK..."
 sudo apt install default-jdk -y
+echo
+echo
 
 echo "Checking Java installation..."
 java -version
+echo
+echo
 
 echo "Installing Git..."
 sudo apt install git -y
+echo
+echo
 
 # Check if javac exists
 if ! command -v javac &> /dev/null
@@ -19,5 +27,7 @@ then
   echo "Try: sudo apt install default-jdk"
   exit 1
 fi
+echo
+echo
 
 echo "Compilation done."
